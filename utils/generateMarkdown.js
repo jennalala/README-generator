38dcd17a428e62660075a5fa108c98a79ee08bb4
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2020-11-04 21:01:14 -0500

    added to generatemarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,36 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## description
+  ${data.description}
+
+  ## table of contents
+
+  *[installation](#installation)
+  *[tests](#tests)
+  *[technology](#technology)
+  *[contributors](#contributors)
+  *[license](#license)
+
+  ## live link
+  ${data.url}
+
+  ## installation
+  ${data.install}
+
+  ## tests
+  ${data.test}
+
+  ## technology
+  ${data.tech}
+
+  ## contributors
+  ${data.contributors}
+
+  ## contact
+  questions please email or checkout github
+  email: ${data.email}
+  Github: https://github.com/${data.github}
 `;
 }
 
