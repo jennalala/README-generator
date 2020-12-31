3e31a85a6125ee85a69b779f1a2dd2f4ce7e8a5e
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2020-12-30 20:19:50 -0500

    still not working

--- index.js
+++ index.js
@@ -1,72 +1,74 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("utils/generateMarkdown.js");
 
-
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
 // array of questions for user
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the name of your project?',
-      },
-      {
-          type: 'input',
-          name: 'githubRepo',
-          message: 'What is the name of your GitHub repo?',
-      },
-      {
-          type: 'input',
-          name: 'description',
-          message: 'describe your repository',
-      },
-      {
-          type: 'input',
-          name: 'how to install',
-          message: 'how do you install?',
-      },
-      {
-          type: 'input',
-          name: 'tech used',
-          message: 'what technology did you use?',
-      },
-      {
-          type: 'input',
-          name: 'url name',
-          message: "what is your URL?",
-      },
-      {
-          type: 'input',
-          name: "contributors",
-          message: "who are the contributors to your repo?",
-      },
-      {
-          type: 'input',
-          name: 'github username',
-          message: 'what is your GitHub user name?',
-      },
-      {
-          type: 'input',
-          name: 'github email',
-          message: 'What is your GiyHub email?'
-      },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe what your project does?",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "How do you install your project?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How do you test this project?",
+  },
+  {
+    type: "input",
+    name: "tech",
+    message: "What technology did you use?",
+  },
+  {
+    type: "input",
+    name: "url",
+    message: "Please share the COMPLETE URL to access your project.",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Who contributed to your project?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username without the @ symbol?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license would you like to use?",
+    choices: ["MIT", "Apache 2.0", "GNU AGPL v3", "Mozilla Public License 2.0"],
+  },
 ];
 
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
+const init = () => {
 
-// function to initialize program
-function init() {
-    inquirer.prompt(questions),then(data)=>{
-        const fileName = `${data.title.toLowerCase().split(" ").join("-")}-README.md`;
-        fs.writeFile(fileName,generateMarkdown(data), (err) => err ? console.error(err) : console.log('Success!'));
-    });
-
+  inquirer.prompt(questions).then((data) => {
+    const fileName = `${data.title
+      .toLowerCase()
+      .split(" ")
+      .join("-")}-README.md`;
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+  });
+};
 
 // function call to initialize program
 init();
-
-
-const writeFileAsync = util.promisify(fs.writeFile);
-}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,37 +1,46 @@
-// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-  ## description
+  
+  ${((license) => {
+    if (license === "MIT")
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    else if (license === "Apache 2.0")
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    else if (license === "Mozilla Public License 2.0")
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    else if (license === "GNU AGPL v3")
+      return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+    else return ``;
+  })(data.license)}
+  ## Description
   ${data.description}
-
-  ## table of contents
-
-  *[installation](#installation)
-  *[tests](#tests)
-  *[technology](#technology)
-  *[contributors](#contributors)
-  *[license](#license)
-
-  ## live link
+  ## Table of Contents
+  
+  * [Installation](#Installation)
+  * [Tests](#Tests)
+  * [Technology](#Technology)
+  * [Contributors](#Contributors)
+  * [License](#License)
+  
+  ## Live Link
   ${data.url}
-
-  ## installation
+  ## Installation
   ${data.install}
-
-  ## tests
+  
+  ## Tests
   ${data.test}
-
-  ## technology
+  
+  ## Technology
   ${data.tech}
-
-  ## contributors
+  ## Contributors
   ${data.contributors}
-
-  ## contact
-  questions please email or checkout github
-  email: ${data.email}
+  
+  ## Contact
+  If you have any questions please email or checkout my github:
+  Email: ${data.email}
   Github: https://github.com/${data.github}
+  ## License
+  ${data.license}
 `;
 }
 
