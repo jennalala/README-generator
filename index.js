1ee425a94f439d7e9d825f7c9eb4a0dca825c2bd
Jenna Lafrancois <jenna.lafrancois@gmail.com>
2021-01-27 12:01:05 -0500

    fixed error in index

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const utils = require("utils");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const questions = [
+  inquirer.prompt([
+
   {
     type: "input",
     name: "title",
@@ -52,24 +54,40 @@ const questions = [
     message: "What is your email address?",
   },
   {
-    type: "list",
-    name: "license",
-    message: "Which license would you like to use?",
-    choices: ["MIT", "Apache 2.0", "GNU AGPL v3", "Mozilla Public License 2.0"],
+    type: "checkbox",
+    name: "licenseBadge",
+    message: "Pick a license for this application.",
+    choices: [
+      {
+        name: "MIT",
+        value: "MIT",
+      },
+      {
+        name: "Apache",
+        value: "Apache",
+      },
+    ],
+    default: ["MIT"],
   },
-];
+  {
+      type: "checkbox",
+      name: "license",
+      message: "Pick a license to display at the bottom of markdown.",
+      choices: [
+        {
+          name: "MIT",
+          value: "https://opensource.org/licenses/MIT",
+        },
+        {
+          name: "Apache",
+          value: "https://www.apache.org/licenses/LICENSE-2.0",
+        },
+      ],
+      default: ["MIT"],
+    },
+]),
 
-const init = () => {
-
-  inquirer.prompt(questions).then((data) => {
-    const fileName = `${data.title
-      .toLowerCase()
-      .split(" ")
-      .join("-")}-README.md`;
-    fs.writeFile(fileName, generateMarkdown(data), (err) =>
-      err ? console.error(err) : console.log("Success!")
-    );
-  });
-};
-
-init();
+questions()
+.then((data) => writeFileAsync("EXAMPLE-README.md", generateMarkdown(data)))
+.then(() => console.log("Successfully wrote to md file"))
+.catch((err) => console.error(err))]
